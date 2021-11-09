6e1c1997721b7be62e480c22bf0bce488e5cbf81
Judyhouser80 <judyhouser80@gmail.com>
2021-11-09 13:23:32 -0500

    changes to functionality

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the Course and learn Vue!',
+            courseGoalA: 'Finish the Course and learn Vue!',
+            courseGoalB: 'Master Vue and build amazing apps!',
             vueLink: 'https://vuejs.org/'
+        }
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
         }
     }
 });
